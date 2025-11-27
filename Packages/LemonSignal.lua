70a86e3cf981e20eb4b2712d26e002698d68420c
AlexeyLegasov63 <61830707+AlexeyLegasov63@users.noreply.github.com>
2025-11-28 01:51:12 +0200

    3.0 - Fully Component Custom Inheritance, Removed Signal and Trove depedencies

--- Packages/LemonSignal.lua
+++
@@ -1,4 +0,0 @@
-local REQUIRED_MODULE = require(script.Parent._Index["kineticwallet_lemon-signal@1.10.0"]["lemon-signal"])
-export type Connection<U...> = REQUIRED_MODULE.Connection<U...>
-export type Signal<T...> = REQUIRED_MODULE.Signal<T...>
-return REQUIRED_MODULE
